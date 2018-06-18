f5875f53f540f7e4513bf241cc3df0fa58c90b47
Robin Harms Oredsson <robin@betahaus.net>
2018-06-18 14:43:31 +0200

    Basic color stuff

--- src/skl_theme/skl_theme/fanstatic_lib.py
+++ src/skl_theme/skl_theme/fanstatic_lib.py
@@ -3,12 +3,12 @@ from arche.interfaces import IViewInitializedEvent
 from fanstatic import Library
 from fanstatic import Resource
 from js.bootstrap import bootstrap_css
-
+from voteit.core.fanstaticlib import voteit_main_css
 
 library = Library('skl_theme', 'static')
 
 skl_custom_bootstrap_css = Resource(library, 'css/bootstrap.css', supersedes=(bootstrap_css,))
-skl_theme_css = Resource(library, 'css/main.css', depends = (skl_custom_bootstrap_css,))
+skl_theme_css = Resource(library, 'css/main.css', depends = (skl_custom_bootstrap_css, voteit_main_css))
 
 
 def need_subscriber(view, event):
