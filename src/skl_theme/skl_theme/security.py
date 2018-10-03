c9d418f985862809f7cab357857c5efb4e25fdc4
Robin Harms Oredsson <robin@betahaus.net>
2018-10-03 16:43:50 +0200

    Button for editing proposals

--- src/skl_theme/skl_theme/security.py
+++ src/skl_theme/skl_theme/security.py
@@ -11,3 +11,5 @@ def includeme(config):
     prop_published.add(vcsec.ROLE_ADMIN, EDIT_EFFECT_CATEGORY)
     prop_published.add(vcsec.ROLE_MODERATOR, EDIT_EFFECT_CATEGORY)
     prop_published.add(vcsec.ROLE_OWNER, EDIT_EFFECT_CATEGORY)
+    # Allow users to edit their proposals within SKL, if the prop is published
+    prop_published.add(vcsec.ROLE_OWNER, vcsec.EDIT)
--- src/skl_theme/skl_theme/views.py
+++ src/skl_theme/skl_theme/views.py
@@ -159,6 +159,21 @@ class EditEffectsForm(DefaultEditForm):
     cancel_success = cancel_failure = cancel
 
 
+def participant_edit_proposal(context, request, va, **kw):
+    if not request.is_moderator and request.has_permission(security.EDIT, context):
+        return """<a href="{url}"
+         title="Redigera förslag"
+         role="button"
+         class="btn btn-default btn-xs"
+         data-placement="bottom"
+         data-external-popover-loaded="false">
+        <span class="text-primary">
+          &nbsp;<span class="glyphicon glyphicon-edit"></span>&nbsp;
+        </span>
+      </a>""".format(url = request.resource_url(context, 'edit'), uid=context.uid)
+#         id="participant-edit-{uid}"
+
+
 def includeme(config):
     config.add_view_action(
         control_panel_link, 'control_panel_proposal', 'effect_settings',
@@ -185,4 +200,11 @@ def includeme(config):
         name='edit_effects',
         permission=EDIT_EFFECT_CATEGORY,
         renderer="arche:templates/form.pt"
+    )
+    config.add_view_action(
+        participant_edit_proposal,
+        'metadata_listing', 'edit_proposal',
+        title="Redigera",
+        interface=IProposal,
+        priority=15,
     )
