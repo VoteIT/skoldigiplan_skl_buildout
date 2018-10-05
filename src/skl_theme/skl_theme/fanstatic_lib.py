556f37b0cdd2f42e207c64819f4902bb209b0d20
Robin Harms Oredsson <robin@betahaus.net>
2018-10-05 07:56:33 +0200

    Needed due to changes in reg form

--- src/skl_theme/skl_theme/fanstatic_lib.py
+++ src/skl_theme/skl_theme/fanstatic_lib.py
@@ -1,5 +1,6 @@
 from arche.interfaces import IBaseView
 from arche.interfaces import IViewInitializedEvent
+from deform_autoneed import need_lib
 from fanstatic import Library
 from fanstatic import Resource
 from js.bootstrap import bootstrap_css
@@ -13,7 +14,8 @@ skl_theme_css = Resource(library, 'css/main.css', depends = (skl_custom_bootstra
 
 def need_subscriber(view, event):
     skl_theme_css.need()
-
+    # Due to changes in the registration schema, the select 2 widget is needed at all times too
+    need_lib('select2')
 
 def includeme(config):
     config.add_subscriber(need_subscriber, [IBaseView, IViewInitializedEvent])
