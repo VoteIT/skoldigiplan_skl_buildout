ebcb0e7929e5601222923cd92fc2eaa5c62e722e
Johan Schiff <johan.schiff@mp.se>
2018-10-18 11:51:06 +0200

    Stats för kommuner och organisationer

--- src/skl_theme/skl_theme/views.py
+++ src/skl_theme/skl_theme/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from collections import Counter
 import re
 
+from arche.views.base import BaseView
 from arche.views.base import DefaultEditForm
 from pyramid.decorator import reify
 from pyramid.httpexceptions import HTTPFound
@@ -10,12 +12,15 @@ from pyramid.httpexceptions import HTTPForbidden
 from pyramid.renderers import render
 from pyramid.response import Response
 from voteit.core import security
+from voteit.core.helpers import get_meeting_participants
 from voteit.core.models.interfaces import IMeeting
 from voteit.core.models.interfaces import IProposal
 from voteit.core.views.control_panel import control_panel_link
 
 from skl_theme.interfaces import IEffectSettings
 from skl_theme.schemas import EditEffectsSchema
+from skl_theme.schemas import get_kommun_values
+from skl_theme.schemas import _organisation_values
 from skl_theme.security import EDIT_EFFECT_CATEGORY
 
 
@@ -174,6 +179,34 @@ def participant_edit_proposal(context, request, va, **kw):
 #         id="participant-edit-{uid}"
 
 
+class ParticipantCriteria(BaseView):
+    @reify
+    def kommun_dict(self):
+        return dict(get_kommun_values())
+
+    @reify
+    def org_dict(self):
+        return dict(_organisation_values)
+
+    @reify
+    def participant_ids(self):
+        return get_meeting_participants(self.context)
+
+    def __call__(self):
+        users = self.root['users']
+        kommuner = Counter()
+        organisationer = Counter()
+        for userid in self.participant_ids:
+            user = users[userid]
+            kommuner[self.kommun_dict.get(user.kommun, 'Okänd')] += 1
+            for org in user.organisation:
+                organisationer[self.org_dict.get(org)] += 1
+        return {
+            'kommuner': kommuner,
+            'organisationer': organisationer,
+        }
+
+
 def includeme(config):
     config.add_view_action(
         control_panel_link, 'control_panel_proposal', 'effect_settings',
@@ -207,4 +240,15 @@ def includeme(config):
         title="Redigera",
         interface=IProposal,
         priority=15,
+    )
+    config.add_view_action(
+        control_panel_link, 'control_panel_participants', 'participant_criteria',
+        title="Kommuner och organisationer", view_name='participant_criteria'
+    )
+    config.add_view(
+        ParticipantCriteria,
+        context=IMeeting,
+        name="participant_criteria",
+        renderer="skl_theme:templates/participant_criteria.pt",
+        permission=security.MODERATE_MEETING
     )
