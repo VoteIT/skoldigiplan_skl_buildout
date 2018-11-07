2b2891525f9bee47c967f4bdca9a8d93d4729c7f
Robin Harms Oredsson <robin@betahaus.net>
2018-11-07 11:40:09 +0100

    Use compat lib

--- src/skl_theme/skl_theme/renderers.py
+++ src/skl_theme/skl_theme/renderers.py
@@ -2,13 +2,11 @@
 from __future__ import unicode_literals
 
 import unicodecsv as csv
-try:
-    from StringIO import StringIO  # python 2
-except ImportError:
-    from io import StringIO  # python 3
+from six import StringIO
 
 
 class CSVRenderer(object):
+
     def __init__(self, info):
         pass
 
@@ -16,20 +14,16 @@ class CSVRenderer(object):
         """ Returns a plain CSV-encoded string with content-type
         ``text/csv``. The content-type may be overridden by
         setting ``request.response.content_type``."""
-
         request = system.get('request')
         if request is not None:
             response = request.response
             ct = response.content_type
             if ct == response.default_content_type:
                 response.content_type = b'text/csv'
-
         fout = StringIO()
         writer = csv.writer(fout)
-
         writer.writerow(value.get('header', []))
         writer.writerows(value.get('rows', []))
-
         return fout.getvalue()
 
 
