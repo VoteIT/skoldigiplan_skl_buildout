e623fea26b5347676130440d9ad61efff90cf857
Robin Harms Oredsson <robin@betahaus.net>
2019-03-04 16:21:02 +0100

    Update install_root.sh

--- install_root.sh
+++ install_root.sh
@@ -2,9 +2,9 @@
 #Ment to be run as root after buildout is done. Will obtain cert and install on nginx debian
 
 cd /etc/nginx
-ln -s /home/voteit/srv/skl_buildout/etc/nginx.conf ./sites-available/skl.conf
-ln -s ./sites-available/skl.conf ./sites-enabled/.
+ln -s /home/voteit/srv/skoldigiplan_skl_buildout/etc/nginx.conf ./sites-available/skoldigiplan-skl.conf
+ln -s ./sites-available/skoldigiplan-skl.conf ./sites-enabled/.
 
 service nginx stop
-certbot certonly --standalone -d skl.voteit.se
+certbot certonly --standalone -d skoldigiplan-skl.voteit.se
 service nginx start
